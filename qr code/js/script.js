5792c8184d9d306f37104af8c14474254a27d8dd
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-04-02 20:38:31 -0300

    qr-code projeto

---
+++ qr code/js/script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = container.querySelector("#qr-form input");
+const qrCodeImg = container.querySelector("#qr-code img");
